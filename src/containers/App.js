381d06976e1aafee1ebf8aff5f2b52df02b223a0
orenmagid <autoimpedicis@gmail.com>
2018-08-14 13:43:48 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import Actors from "../components/Actors";
+import Directors from "../components/Directors";
+import Movies from "../components/Movies";
 
-
-const App = (props) => {
+const App = props => {
   return (
     <Router>
-      {/*{code here}*/}
+      <React.Fragment>
+        <NavBar />
+
+        <Route exact path="/" render={Home} />
+        <Route path="/movies" render={Movies} />
+        <Route path="/directors" render={Directors} />
+        <Route path="/actors" render={Actors} />
+      </React.Fragment>
     </Router>
   );
 };
 
-export default App
+export default App;
